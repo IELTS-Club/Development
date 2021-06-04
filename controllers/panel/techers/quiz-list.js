5ba96bc13bb7aa9078863c41bbf67340cf06a901
Alireza Jafari <asjn3e@gmail.com>
2021-06-04 13:54:31 +0430

    alter the class quizzes routes with :id parameter

--- controllers/panel/techers/quiz-list.js
+++ controllers/panel/techers/quiz-list.js
@@ -4,7 +4,7 @@ const isLogedIn=require("../../../middleware/isLogedIn");
 const isConfirmed=require("../../../middleware/isConfirmed");
 const isTeacher=require("../../../middleware/isTeacher");
 
-panel.get("/teachers/quiz-list",[isLogedIn,isConfirmed,isTeacher],async(req,res)=>{
+panel.get("/teachers/quiz-list/:id",[isLogedIn,isConfirmed,isTeacher],async(req,res)=>{
     res.render("panel/teachers/quiz-list",{
         userName:req.user.name
     })
