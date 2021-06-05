9814d8419eb60d805550aeb848a11461e2552b32
Alireza Jafari <asjn3e@gmail.com>
2021-06-05 16:15:31 +0430

    complete and mdify fetch api in js frontend and redirect to exam basic page

--- controllers/panel/techers/quiz-list.js
+++ controllers/panel/techers/quiz-list.js
@@ -4,13 +4,26 @@ const isLogedIn=require("../../../middleware/isLogedIn");
 const isConfirmed=require("../../../middleware/isConfirmed");
 const isTeacher=require("../../../middleware/isTeacher");
 
-panel.get("/teachers/quiz-list/:id",[isLogedIn,isConfirmed,isTeacher],async(req,res)=>{
+panel.get("/teachers/quiz-list/:classId",[isLogedIn,isConfirmed,isTeacher],async(req,res)=>{
     res.render("panel/teachers/quiz-list",{
-        userName:req.user.name
+        userName:req.user.name,
+        classId:req.params.classId
     })
 })
-panel.post("/teachers/quiz-list/:id",[isLogedIn,isConfirmed,isTeacher],async(req,res)=>{
+panel.post("/teachers/quiz-list/:classId",[isLogedIn,isConfirmed,isTeacher],async(req,res)=>{
+    let body=req.body;    
+    req.session.Title=body.examTitle;
+    req.session.Type=body.examType;
+    req.session.StartDate=body.examStartDate;
+    req.session.StartHour=body.examStartHour;
+    req.session.StopDate=body.examStoptDate;
+    req.session.StopHour=body.examStoptHour;
+    req.session.QuestionsNumber=body.QuestionsNumber;
     
-})
+    
+});
 
+panel.get("/hello",[isLogedIn,isConfirmed,isTeacher],async(req,res)=>{
+    res.send("صفحه آزمون")
+})
 module.exports=panel;
